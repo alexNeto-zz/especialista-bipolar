eedb5b381e2651ec460ba3f93137bbca8edb9dda
alexNeto <alex.neto3@hotmail.com>
2019-03-17 15:34:49 -0300

    consertado logica

--- especialista-back/bipolar/bipolar.py
+++ especialista-back/bipolar/bipolar.py
@@ -1,11 +1,11 @@
 import operator
 from functools import reduce
 
-from pyknow import *
+from pyknow import TEST, AND, OR, NOT, Rule, Fact, KnowledgeEngine, W
 
 
 def somatorio(lista_sintomas):
-    return reduce(operator.add, lista_sintomas)
+    return reduce(operator.add, map(lambda i: 0 if i is None else i, lista_sintomas))
 
 
 def ask(question):
@@ -249,160 +249,136 @@ class Bipolar(KnowledgeEngine):
         print(value)
         self.declare(Sintomas(prejuiso_area_importancia=value))
 
+    """
+    Verificação das regras 
+    """
 
-"""
-Verificação das regras 
-"""
+    @Rule(AND(Sintomas(humor_anormal=1), Sintomas(fisiologico=1)))
+    def mania(self):
+        self.declare(Sintomas(mania=1))
+        print("mania")
 
+    @Rule(OR(Sintomas(prejuizo_social=1), Sintomas(prejuiso_profissional=1)))
+    def prejuiso_acentuado(self):
+        self.declare(Sintomas(prejuizo_acentuado=1))
+        print("prejuizo acentuado")
 
-@Rule(AND(Sintomas(humor_anormal=1), Sintomas(fisiologico=1)))
-def mania(self):
-    self.declare(Sintomas(mania=1))
-    print("mania")
+    @Rule(OR(Sintomas(prejuizo_si=1), Sintomas(prejuizo_outros=1)))
+    def risco_potencial(self):
+        self.declare(Sintomas(risco_potencial=1))
+        print("risco potencial")
 
+    @Rule(OR(OR(Sintomas(prejuizo_acentuado=1), Sintomas(risco_potencial=1)), Sintomas(psicose=1)))
+    def perturbacao_humor(self):
+        self.declare(Sintomas(perturbacao_humor=1))
+        print("perturbacao_humor")
 
-@Rule(OR(Sintomas(prejuizo_social=1), Sintomas(prejuiso_profissional=1)))
-def prejuiso_acentuado(self):
-    self.declare(Sintomas(prejuizo_acentuado=1))
-    print("prejuizo acentuado")
+    @Rule(OR(Sintomas(autoestima_inflada=1), Sintomas(grandiosidade=1)))
+    def autoestima_excessiva(self):
+        self.declare(Sintomas(autoestima_excessiva=1))
+        print("autoestima_excessiva")
 
+    @Rule(OR(Sintomas(loquaz=1), Sintomas(pressao_continuar_falando=1)))
+    def aumento_fala(self):
+        self.declare(Sintomas(aumento_fala=1))
+        print("aumento_fala")
 
-@Rule(OR(Sintomas(prejuizo_si=1), Sintomas(prejuizo_outros=1)))
-def risco_potencial(self):
-    self.declare(Sintomas(risco_potencial=1))
-    print("risco potencial")
+    @Rule(OR(Sintomas(fuga_ideias=1), Sintomas(pensamento_acelerado=1)))
+    def mudanca_modo_pensar(self):
+        self.declare(Sintomas(mudanca_modo_pensar=1))
+        print("mudanca_modo_pensar")
 
+    @Rule(OR(Sintomas(aumento_atividade_objetivo=1), Sintomas(agitacao_psicomotora=1)))
+    def agitacao(self):
+        self.declare(Sintomas(agitacao=1))
+        print("Agitacao")
 
-@Rule(OR(OR(Sintomas(prejuizo_acentuado=1), Sintomas(risco_potencial=1)), Sintomas(psicose=1)))
-def perturbacao_humor(self):
-    self.declare(Sintomas(perturbacao_humor=1))
-    print("perturbacao_humor")
+    @Rule(TEST(somatorio([
+        Sintomas().get("autoestima_excessiva"),
+        Sintomas().get("reducao_sono"),
+        Sintomas().get("aumento_fala"),
+        Sintomas().get("mudanca_modo_pensar"),
+        Sintomas().get("distrabilidade"),
+        Sintomas().get("agitacao"),
+        Sintomas().get("envolvimento_atividade_risco")
+    ]) >= 3))
+    def mudanca_comportamental(self):
+        self.declare(Sintomas(mudanca_comportamental=1))
+        print("mudanca_comportamental")
 
+    @Rule(AND(Sintomas(mania=1), Sintomas(psicose=1)))
+    def hipomania(self):
+        self.declare(Sintomas(hipomania=1))
 
-@Rule(OR(Sintomas(autoestima_inflada=1), Sintomas(grandiosidade=1)))
-def autoestima_excessiva(self):
-    Sintomas(autoestima_excessiva=1)
-    print("autoestima_excessiva")
+    @Rule(OR(Sintomas(humor_deprimido=1), Sintomas(irritavel=1)))
+    def humor_deprimido(self):
+        self.declare(Sintomas(humor_deprimido=1))
 
+    @Rule(OR(Sintomas(perda_interesse=1), Sintomas(perda_prazer=1)))
+    def perda_interesse(self):
+        self.declare(Sintomas(perda_interesse=1))
 
-@Rule(OR(Sintomas(loquaz=1), Sintomas(pressao_continuar_falando=1)))
-def aumento_fala(self):
-    Sintomas(aumento_fala=1)
+    @Rule(OR(OR(Sintomas(perda_peso=1), Sintomas(ganho_peso=1)),
+             OR(Sintomas(reducao_alimentacao=1), Sintomas(aumento_alimentacao=1))))
+    def alteracao_alimentacao(self):
+        self.declare(Sintomas(Sintomas(alteracao_alimentacao=1)))
 
+    @Rule(OR(Sintomas(insonia=1), Sintomas(hipersonia=1)))
+    def alteracao_sono(self):
+        self.declare(Sintomas(alteracao_sono=1))
 
-@Rule(OR(Sintomas(fuga_ideias=1), Sintomas(pensamento_acelerado=1)))
-def mudanca_modo_pensar(self):
-    Sintomas(mudanca_modo_pensar=1)
-    print("mudanca_modo_pensar")
+    @Rule(OR(Sintomas(agitacao=1), Sintomas(retardo_psicomotor=1)))
+    def alteracao_comportamentao(self):
+        self.declare(Sintomas(alteracao_comportamentao=1))
 
+    @Rule(OR(Sintomas(fadiga=1), Sintomas(perda_energia=1)))
+    def cansaco(self):
+        self.declare(Sintomas(cansaco=1))
 
-@Rule(Sintomas(aumento_atividade_objetivo=1), Sintomas(agitacao_psicomotora=1))
-def agitacao(self):
-    Sintomas(agitacao=1)
-    print("Agitacao")
+    @Rule(OR(Sintomas(inutilidade=1), Sintomas(culpa_excessiva=1), Sintomas(culpa_inapropriada=1)))
+    def sentimento_depressivo(self):
+        self.declare(Sintomas(sentimento_depressivo=1))
 
+    @Rule(OR(Sintomas(capacidade_diminuida=1), Sintomas(indecisao=1)))
+    def alteracao_pensamento(self):
+        self.declare(Sintomas(alteracao_pensamento=1))
 
-@Rule(TEST(somatorio([
-    MATCH.autoestima_excessiva,
-    MATCH.reducao_sono,
-    MATCH.aumento_fala,
-    MATCH.mudanca_modo_pensar,
-    MATCH.distrabilidade,
-    MATCH.agitacao,
-    MATCH.envolvimento_atividade_risco
-]) >= 3))  # TODO
-def mudanca_comportamental(self):
-    Sintomas(mudanca_comportamental=1)
-    print("mudanca_comportamental")
+    @Rule(AND(OR(Sintomas(humor_deprimido=1), Sintomas(perda_interesse=1)), TEST(somatorio([
+        Sintomas().get("humor_deprimido"),
+        Sintomas().get("perda_interesse"),
+        Sintomas().get("alteracao_alimentacao"),
+        Sintomas().get("alteracao_sono"),
+        Sintomas().get("alteracao_comportamentao"),
+        Sintomas().get("cansaco"),
+        Sintomas().get("sentimento_depressivo"),
+        Sintomas().get("alteracao_pensamento"),
+        Sintomas().get("pensamentos_morte")
+    ]) >= 5)))
+    def sintomas_depressivos(self):
+        self.declare(Sintomas(sintomas_depressivos=1))
 
+    @Rule(OR(OR(Sintomas(sofrimento_clinico=1), Sintomas(prejuiso_social=1)),
+             OR(Sintomas(prejuiso_profissional=1), Sintomas(prejuiso_area_importancia=1))))
+    def transtorno(self):
+        self.declare(Sintomas(transtorno=1))
 
-@Rule(AND(Sintomas(mania=1), Sintomas(psicose=1)))
-def hipomania(self):
-    self.declare(Sintomas(hipomania=1))
+    @Rule(AND(Sintomas(sintomas_depressivos=1), Sintomas(fisiologico=1)))
+    def depressao(self):
+        self.declare(Sintomas(depressao=1))
 
+    @Rule(AND(Sintomas(mania=1), Sintomas(depressao=1)))
+    def bipolar_i(self):
+        self.declare(Sintomas(bipolar_i=1))
 
-@Rule(OR(Sintomas(humor_deprimido=1), Sintomas(irritavel=1)))
-def humor_deprimido(self):
-    self.declare(Sintomas(humor_deprimido=1))
+    @Rule(AND(Sintomas(hipomania=1), Sintomas(depressao=1)))
+    def bipolar_ii(self):
+        self.declare(Sintomas(bipolar_ii=1))
 
-
-@Rule(OR(Sintomas(perda_interesse=1), Sintomas(perda_prazer=1)))
-def perda_interesse(self):
-    self.declare(Sintomas(perda_interesse=1))
-
-
-@Rule(OR(OR(Sintomas(perda_peso=1), Sintomas(ganho_peso=1)),
-         OR(Sintomas(reducao_alimentacao=1), Sintomas(aumento_alimentacao=1))))
-def alteracao_alimentacao(self):
-    self.declare(Sintomas(Sintomas(alteracao_alimentacao=1)))
-
-
-@Rule(OR(Sintomas(insonia=1), Sintomas(hipersonia=1)))
-def alteracao_sono(self):
-    self.declare(Sintomas(alteracao_sono=1))
-
-
-@Rule(OR(Sintomas(agitacao=1), Sintomas(retardo_psicomotor=1)))
-def alteracao_comportamentao(self):
-    self.declare(Sintomas(alteracao_comportamentao=1))
-
-
-@Rule(OR(Sintomas(fadiga=1), Sintomas(perda_energia=1)))
-def cansaco(self):
-    self.declare(Sintomas(cansaco=1))
-
-
-@Rule(OR(Sintomas(inutilidade=1), Sintomas(culpa_excessiva=1), Sintomas(culpa_inapropriada=1)))
-def sentimento_depressivo(self):
-    self.declare(Sintomas(sentimento_depressivo=1))
-
-
-@Rule(OR(Sintomas(capacidade_diminuida=1), Sintomas(indecisao=1)))
-def alteracao_pensamento(self):
-    self.declare(Sintomas(alteracao_pensamento=1))
-
-
-@Rule(AND(OR(Sintomas(humor_deprimido=1), Sintomas(perda_interesse=1)), TEST(somatorio([
-    MATCH.humor_deprimido,
-    MATCH.perda_interesse,
-    MATCH.alteracao_alimentacao,
-    MATCH.alteracao_sono,
-    MATCH.alteracao_comportamentao,
-    MATCH.cansaco,
-    MATCH.sentimento_depressivo,
-    MATCH.alteracao_pensamento,
-    MATCH.pensamentos_morte
-]) >= 5)))
-def sintomas_depressivos(self):
-    self.declare(Sintomas(sintomas_depressivos=1))
-
-
-@Rule(OR(OR(Sintomas(sofrimento_clinico=1), Sintomas(prejuiso_social=1)),
-         OR(Sintomas(prejuiso_profissional=1), Sintomas(prejuiso_area_importancia=1))))
-def transtorno(self):
-    self.declare(Sintomas(transtorno=1))
-
-
-@Rule(AND(Sintomas(sintomas_depressivos=1), Sintomas(fisiologico=1)))
-def depressao(self):
-    self.declare(Sintomas(depressao=1))
-
-
-@Rule(AND(Sintomas(mania=1), Sintomas(depressao=1)))
-def bipolar_i(self):
-    self.declare(Sintomas(bipolar_i=1))
-
-
-@Rule(AND(Sintomas(hipomania=1), Sintomas(depressao=1)))
-def bipolar_ii(self):
-    self.declare(Sintomas(bipolar_ii=1))
-
-
-@Rule(AND(OR(Sintomas(pensamentos_morte=1), Sintomas(psicose=1), Sintomas(transtorno=1)),
-          Sintomas(mania=0), Sintomas(hipomania=0), Sintomas(depressao=0)))
-def outro_transtorno(self):
-    self.declare(Sintomas(outro_transtorno=1))
+    @Rule(AND(OR(Sintomas(pensamentos_morte=1), Sintomas(psicose=1), Sintomas(transtorno=1)),
+              Sintomas(mania=0), Sintomas(hipomania=0), Sintomas(depressao=0)))
+    def outro_transtorno(self):
+        self.declare(Sintomas(outro_transtorno=1))
 
 
 if __name__ == "__main__":
-    print("maaaaaain")
+    print(Sintomas().get("mania"))
